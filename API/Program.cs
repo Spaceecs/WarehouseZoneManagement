09c0cbad4bafb6fa30f168973eedabc64e46e7fd
Spaceecs <bocianovski.ol@e-u.edu.ua>
2026-07-11 14:04:10 +0300

    Add global exception handler to return proper 400 responses for validation errors

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using Application.Warehouses.Queries;
 using Application.Warehouses.Validators;
 using FluentValidation;
 using Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -26,6 +27,29 @@ builder.Services.AddValidatorsFromAssemblyContaining<CreateWarehouseValidator>()
 builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfiles>());
 
 var app = builder.Build();
+
+app.UseExceptionHandler(builder =>
+{
+    builder.Run(async context =>
+    {
+        var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>();
+        var exception = exceptionFeature?.Error;
+
+        context.Response.ContentType = "application/json";
+
+        if (exception is ValidationException validationException)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var errors = string.Join("; ", validationException.Errors.Select(e => e.ErrorMessage));
+            await context.Response.WriteAsJsonAsync(new { error = errors });
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(new { error = "An unexpected error occurred" });
+        }
+    });
+});
 
 if (app.Environment.IsDevelopment())
 {
